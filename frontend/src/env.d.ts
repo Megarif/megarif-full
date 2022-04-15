1fe47c184dcb56bae11afab3f685feeae947a901
cclarice <179danil@gmail.com>
2022-04-16 00:00:59 +0300

    Router Meta layouts types added

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -1,8 +1,17 @@
 /// <reference types="vite/client" />
+import { Layout } from './layouts'
 
 declare module '*.vue' {
+  // @ts-ignore
   import type { DefineComponent } from 'vue'
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
   const component: DefineComponent<{}, {}, any>
   export default component
+}
+
+declare module 'vue-router' {
+  // eslint-disable-next-line no-unused-vars
+  interface RouteMeta {
+    layout: Layout
+  }
 }
