5fc6e0cd3e1f7768ec95002af0c8d9eea2afc3c5
cclarice <179danil@gmail.com>
2022-04-16 00:01:08 +0300

    Pages init

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <h1>
+      Домашняя страница
+    </h1>
+  </div>
+</template>
+
+<script
+  setup
+  lang="ts"
+>
+
+</script>
+
+<style
+  lang="scss"
+  scoped
+>
+
+</style>
