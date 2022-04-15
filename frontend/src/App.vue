d40b21637b2ddb8002b565bd1109355100978006
cclarice <179danil@gmail.com>
2022-04-15 23:54:29 +0300

    Layouts added

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,32 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <component :is="layout" />
 </template>
 
+<script
+  setup
+  lang="ts"
+>
+import { computed } from 'vue'
+import layouts from '@/layouts'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+const layout = computed(() => {
+  if (route.meta.layout) {
+    return route.meta.layout
+  }
+  return layouts.empty
+})
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
+#megarif {
+  font-family:             Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing:  antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  text-align:              center;
+  color:                   #2C3E50;
+  margin-top:              60px;
 }
 </style>
---
+++ frontend/src/layouts/EmptyLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <router-view />
+</template>
+
+<script
+  setup
+  lang="ts"
+>
+
+</script>
+
+<style
+  lang="scss"
+  scoped
+>
+
+</style>
