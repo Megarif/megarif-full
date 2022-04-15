0fd853cfa4808492b5624a4deb7cae0971767c2e
cclarice <179danil@gmail.com>
2022-04-16 00:00:37 +0300

    InputText.vue created

---
+++ frontend/src/components/InputText.vue
@@ -0,0 +1,36 @@
+<template>
+  <label class="input-text">
+    <input
+      :type="type"
+      :value="props.modelValue"
+      :disabled="props.disabled"
+      :placeholder="props.placeholder"
+    >
+  </label>
+</template>
+
+<script
+  setup
+  lang="ts"
+>
+import { computed } from 'vue'
+
+interface Props {
+  modelValue: string
+  type?: 'text' | 'password' | 'email'
+  placeholder?: string
+  disabled?: boolean
+}
+
+const props = defineProps<Props>()
+const type = computed<Props['type']>(() => {
+  return props.type || 'text'
+})
+</script>
+
+<style
+  lang="scss"
+  scoped
+>
+
+</style>
