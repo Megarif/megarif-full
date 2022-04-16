d050208215e0bb3b4ad309189fb0db956acd099d
cclarice <179danil@gmail.com>
2022-04-16 16:44:50 +0300

    MainLayout update

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,12 +1,22 @@
 <template>
   <header>
-    header
+    <nav>
+      <h1>
+        Мәгариф
+      </h1>
+      <choose-lang />
+    </nav>
   </header>
   <main>
     <router-view />
   </main>
   <footer>
-    footer
+    <nav>
+      <router-link
+        v-for="route of routes"
+        :key="route"
+      />
+    </nav>
   </footer>
 </template>
 
@@ -14,7 +24,34 @@
   setup
   lang="ts"
 >
-
+import ChooseLang from '@/components/ChooseLang.vue'
+const routes = [
+  {
+    name: 'home',
+    link: '/',
+    icon: ''
+  },
+  {
+    name: 'lessons',
+    link: '/lessons',
+    icon: ''
+  },
+  {
+    name: 'events',
+    link: '/events',
+    icon: ''
+  },
+  {
+    name: 'market',
+    link: '/market',
+    icon: ''
+  },
+  {
+    name: 'bookmarks',
+    link: '/bookmarks',
+    icon: ''
+  }
+]
 </script>
 
 <style
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,5 @@
 <template>
   <user-block />
-  <header>
-    <nav>
-      <h1>
-        Мәгариф
-      </h1>
-      <choose-lang />
-    </nav>
-  </header>
-  <footer>
-    <nav>
-
-    </nav>
-  </footer>
 </template>
 
 <script
@@ -20,10 +7,6 @@
   lang="ts"
 >
 import UserBlock from '@/components/UserBlock.vue'
-import useUser from '@/store/useUser'
-import ChooseLang from '@/components/ChooseLang.vue'
-
-const user = useUser()
 </script>
 
 <style
